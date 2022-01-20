695eb1aff534ab9f45848db5555d46563b429f9a
Irene <iserrano@mill.edu>
2022-01-19 17:09:51 -0800

    Begin spinner implementation

--- app/src/main/java/com/example/mindyourpet2/AddPetActivity.kt
+++ app/src/main/java/com/example/mindyourpet2/AddPetActivity.kt
@@ -2,10 +2,36 @@ package com.example.mindyourpet2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class AddPetActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_pet)
+        val spinner: Spinner = findViewById(R.id.pet_species)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.species,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        //item was selected, you can retrieve the selected item using
+
+        parent.getItemAtPosition(pos)
+    }
+    override fun onNothingSelected(parent: AdapterView<*>) {
+
     }
 }
